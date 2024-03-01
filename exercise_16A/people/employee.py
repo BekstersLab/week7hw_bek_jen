9cbb8a88642d834491603b201173bfe608a82b5b
Bek <rebecca.e.rootes@gmail.com>
2024-03-01 21:16:18 +0000

    Resolving conflicts between main branch and jen_branch

--- exercise_16A/people/employee.py
+++ exercise_16A/people/employee.py
@@ -2,7 +2,7 @@ from exercise_16A.people.person import Person
 
 
 class Employee(Person):
-    def __init__(self, firstname, middlename, lastname, email, id_number, department):
+    def __init__(self, firstname, middlename, lastname, email, id_number, department, leave_days):
         # use super() to refer to the base or parent class
         # in this case we use super() to access the __init__ method from the Person class
         # calls the constructor function (__init__) from the parent class
@@ -10,6 +10,7 @@ class Employee(Person):
         super().__init__(firstname, middlename, lastname, email)  # Call the parent class constructor
         self.id_number = id_number  # Unique to Employee
         self.department = department  # Unique to Employee
+        self.leave_days = leave_days
 
     def get_id_number(self):
         return self.id_number
@@ -17,6 +18,12 @@ class Employee(Person):
     def get_department(self):
         return self.department
 
+    def __add__(self, other):
+        if isinstance(other, (int, float)) and self.leave_days + other >= 0:
+            self.leave_days += other
+        else:
+            print("Error")
+        return self
 
 
 
