8c9c30b986813e63826931c5a9ef363cab0d0ba6
Bek <rebecca.e.rootes@gmail.com>
2024-03-02 22:05:01 +0000

    changes to variable names, + and - overload operators and polymorphism

--- exercise_16A/app.py
+++ exercise_16A/app.py
@@ -9,28 +9,57 @@ person_middlename = person_1.get_middlename()
 person_lastname = person_1.get_lastname()
 person_email = person_1.get_email()
 
-print(f"{person_firstname} {person_lastname}'s email address is {person_email}")
-# Mary Peterson's email address is mary.peterson@gmail.com
-
+# print(f"{person_firstname} {person_lastname}'s email address is {person_email}")
+# # Mary Peterson's email address is mary.peterson@gmail.com
 
 # instance of Employee class
-employee_1 = Employee('Robert', 'Edward', 'Jones', 'robert.jones@gmail.com',
-                      'EM4505', 'sales')
+r_jones = Employee('Robert', 'Edward', 'Jones', 'robert.jones@gmail.com',
+                   'EM4505', 'sales', 'JB 56 73 80 C', 20)
 
-employee_firstname = employee_1.get_firstname()
-employee_middlename = employee_1.get_middlename()
-employee_lastname = employee_1.get_lastname()
-employee_email = employee_1.get_email()
-employee_id = employee_1.get_id_number()
-employee_department = employee_1.get_department()
+employee_firstname = r_jones.get_firstname()
+employee_middlename = r_jones.get_middlename()
+employee_lastname = r_jones.get_lastname()
+employee_email = r_jones.get_email()
+employee_id = r_jones.get_id_number()
+employee_department = r_jones.get_department()
+employee_national_insurance = r_jones.get_national_insurance()
+employee_leave_days = r_jones.get_leave_days()
 
 print(f"{employee_firstname} {employee_middlename} {employee_lastname} works in the {employee_department} department")
 # Robert Edward Jones works in the sales department
 
-print(f"{employee_firstname}'s ID number is {employee_id}")  # Robert's ID number is EM4505
+print(f"{employee_firstname}'s employee ID number is {employee_id} and his national insurance number is {employee_national_insurance}")
+# Robert's employee ID number is EM4505 and his national insurance number is JB 56 73 80 C
 
-customer_1 = Customer('Helen', 'Claire', 'Herbert', 'helen.herbert@gmail.com', '85647239')
-# use __add__ in customer.py to join attributes together into one variable which is easier to call
-customer_fullname = customer_1.first_name + " " + customer_1.middle_name + " " + customer_1.get_lastname()
-customer_account = customer_1.get_account_number()
-print(f"{customer_fullname}'s account number is: {customer_1.get_account_number()}")
+print(f"{r_jones.first_name} {r_jones.get_lastname()} has {r_jones.leave_days} leave days remaining")
+# Robert Jones has 20 leave days remaining
+
+# using the overload add operator to add leave days to existing amount
+r_jones + 6
+print(f"{r_jones.first_name} now has {r_jones.leave_days} leave days remaining")
+# Robert now has 26 leave days remaining
+
+# using the overload subtract operator to deduct leave days
+r_jones - 30
+# taking more leave than remaining shows 0.
+# this could be fixed to give an error message if he takes more than remaining amount
+print(f"{r_jones.first_name} has {r_jones.leave_days} leave days remaining")
+# Robert has 0 leave days remaining
+
+employee_display_info = r_jones.display_info()
+# polymorphism - printing info using display_info method from Employee class
+print(f"{employee_display_info}")
+# Name: Robert Edward Jones - from display_info in Person class
+# Email: robert.jones@gmail.com - from display_info in Person class
+
+s_harris = Employee('Sarah', 'Louise', 'Harris', 'sarah.harris@gmail.com',
+                    'EM0306', 'accounts', 'GE 56 38 61 D', 5)
+
+employee_display_info = s_harris.display_info()
+print(f"{employee_display_info}")
+
+# customer_1 = Customer('Helen', 'Claire', 'Herbert', 'helen.herbert@gmail.com', '85647239')
+# # use __add__ in customer.py to join attributes together into one variable which is easier to call
+# customer_fullname = customer_1.first_name + " " + customer_1.middle_name + " " + customer_1.get_lastname()
+# customer_account = customer_1.get_account_number()
+# print(f"{customer_fullname}'s account number is: {customer_1.get_account_number()}")
--- exercise_16A/people/employee.py
+++ exercise_16A/people/employee.py
@@ -1,16 +1,26 @@
-from people.person import Person
+from exercise_16A.people.person import Person
 
 
 class Employee(Person):
-    def __init__(self, firstname, middlename, lastname, email, id_number, department, leave_days):
-        # use super() to refer to the base or parent class
+    def __init__(self, firstname, middlename, lastname, email, id_number, department, national_insurance, leave_days=0):
+        # use super() to refer to the base class
         # in this case we use super() to access the __init__ method from the Person class
         # calls the constructor function (__init__) from the parent class
         # Employee now uses parent class (Person) but adds its own stuff on
         super().__init__(firstname, middlename, lastname, email)  # Call the parent class constructor
         self.id_number = id_number  # Unique to Employee
         self.department = department  # Unique to Employee
+        self.__national_insurance = national_insurance
         self.leave_days = leave_days
+
+    # polymorphism - this calls the base class method and adds further information
+    def display_info(self):
+        base_info = Person.display_info(self)
+        return (f"{base_info}"
+                f"\nID Number: {self.get_id_number()}"
+                f"\nDepartment: {self.get_department()}"
+                f"\nNI Number: {self.get_national_insurance()}"
+                f"\nLeave Days: {self.get_leave_days()}")
 
     def get_id_number(self):
         return self.id_number
@@ -18,19 +28,18 @@ class Employee(Person):
     def get_department(self):
         return self.department
 
+    def get_national_insurance(self):
+        return self.__national_insurance
+
     def __add__(self, other):
-        if isinstance(other, (int, float)) and self.leave_days + other >= 0:
-            self.leave_days += other
-        else:
-            print("Error")
+        self.leave_days += other
         return self
 
+    def __sub__(self, other):
+        self.leave_days -= other
+        if self.leave_days < 0:
+            self.leave_days = 0
+        return self
 
-
-
-
-
-
-
-
-
+    def get_leave_days(self):
+        return self.leave_days
