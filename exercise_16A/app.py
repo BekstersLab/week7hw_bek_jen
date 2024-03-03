b1b74350e8155fd667b73d3ea6c195c12c3144b3
Bek <rebecca.e.rootes@gmail.com>
2024-03-03 16:41:32 +0000

    update to comments

--- exercise_16A/app.py
+++ exercise_16A/app.py
@@ -12,10 +12,12 @@ person_email = person_1.get_email()
 # print(f"{person_firstname} {person_lastname}'s email address is {person_email}")
 # # Mary Peterson's email address is mary.peterson@gmail.com
 
-# instance of Employee class
+# create an instance of Employee class
 r_jones = Employee('Robert', 'Edward', 'Jones', 'robert.jones@gmail.com',
                    'EM4505', 'sales', 'JB 56 73 80 C', 20)
 
+# instance (r_jones) - call getter to get value and assign to a variable
+# the variables can then be referenced in an f-string to print
 employee_firstname = r_jones.get_firstname()
 employee_middlename = r_jones.get_middlename()
 employee_lastname = r_jones.get_lastname()
@@ -25,12 +27,15 @@ employee_department = r_jones.get_department()
 employee_national_insurance = r_jones.get_national_insurance()
 employee_leave_days = r_jones.get_leave_days()
 
+# initially started calling the defined variables above...
 print(f"{employee_firstname} {employee_middlename} {employee_lastname} works in the {employee_department} department")
 # Robert Edward Jones works in the sales department
 
-print(f"{employee_firstname}'s employee ID number is {employee_id} and his national insurance number is {employee_national_insurance}")
+print(f"{employee_firstname}'s employee ID number is {employee_id} and his national insurance number is "
+      f"{employee_national_insurance}")
 # Robert's employee ID number is EM4505 and his national insurance number is JB 56 73 80 C
 
+# changed way of referencing variable, now use instance variable name with the method from employee.py
 print(f"{r_jones.first_name} {r_jones.get_lastname()} has {r_jones.leave_days} leave days remaining")
 # Robert Jones has 20 leave days remaining
 
@@ -46,6 +51,7 @@ r_jones - 30
 print(f"{r_jones.first_name} has {r_jones.leave_days} leave days remaining")
 # Robert has 0 leave days remaining
 
+# set a generic variable to reference each employees information as defined in display_info method
 employee_display_info = r_jones.display_info()
 # polymorphism - printing info using display_info method from Employee class
 print(f"{employee_display_info}")
@@ -55,9 +61,11 @@ print(f"{employee_display_info}")
 s_harris = Employee('Sarah', 'Louise', 'Harris', 'sarah.harris@gmail.com',
                     'EM0306', 'accounts', 'GE 56 38 61 D', 5)
 
+# reassign the variable with a new instance (s_harris)
 employee_display_info = s_harris.display_info()
 print(f"{employee_display_info}")
 
+# DO NOT USE - incorrect use of operator overloading
 # customer_1 = Customer('Helen', 'Claire', 'Herbert', 'helen.herbert@gmail.com', '85647239')
 # # use __add__ in customer.py to join attributes together into one variable which is easier to call
 # customer_fullname = customer_1.first_name + " " + customer_1.middle_name + " " + customer_1.get_lastname()
--- exercise_16A/people/employee.py
+++ exercise_16A/people/employee.py
@@ -2,15 +2,17 @@ from exercise_16A.people.person import Person
 
 
 class Employee(Person):
+    # add instance attributes after self, they can be customised after creating the instance
     def __init__(self, firstname, middlename, lastname, email, id_number, department, national_insurance, leave_days=0):
         # use super() to refer to the base class
         # in this case we use super() to access the __init__ method from the Person class
-        # calls the constructor function (__init__) from the parent class
-        # Employee now uses parent class (Person) but adds its own stuff on
-        super().__init__(firstname, middlename, lastname, email)  # Call the parent class constructor
-        self.id_number = id_number  # Unique to Employee
-        self.department = department  # Unique to Employee
-        self.__national_insurance = national_insurance
+        # calls the constructor function (__init__) from the base class
+        # Employee now uses base class (Person) but adds its own stuff on
+        super().__init__(firstname, middlename, lastname, email)  # Call the base class constructor
+        # Unique to Employee:
+        self.id_number = id_number
+        self.department = department
+        self.__national_insurance = national_insurance  # __ before to keep it private
         self.leave_days = leave_days
 
     # polymorphism - this calls the base class method and adds further information
@@ -28,13 +30,19 @@ class Employee(Person):
     def get_department(self):
         return self.department
 
+    # use a getter as national insurance attribute is private
+    # work with attributes indirectly as they can be accessed from outside the class directly through the getter
+    # getters don't take arguments, they just return a value
     def get_national_insurance(self):
+        # return value of the national insurance instance attribute when the method is called
         return self.__national_insurance
 
+    # operator overloading to add leave days (+ operator)
     def __add__(self, other):
         self.leave_days += other
         return self
 
+    # operator overloading to subtract leave days (- operator)
     def __sub__(self, other):
         self.leave_days -= other
         if self.leave_days < 0:
