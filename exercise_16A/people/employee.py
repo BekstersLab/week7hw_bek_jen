f1169d15551f5e4605d16fd7fec4888299c2e201
Bek <rebecca.e.rootes@gmail.com>
2024-03-03 18:16:20 +0000

    deleting duplicate code from bad merge

--- exercise_16A/people/employee.py
+++ exercise_16A/people/employee.py
@@ -2,16 +2,6 @@ from exercise_16A.people.person import Person
 
 
 class Employee(Person):
-
-    def __init__(self, firstname, middlename, lastname, email, id_number, department):
-        # use super() to refer to the base or parent class
-        # in this case we use super() to access the __init__ method from the Person class
-        # calls the constructor function (__init__) from the parent class
-        # Employee now uses parent class (Person) but adds its own stuff on
-        super().__init__(firstname, middlename, lastname, email)  # Call the parent class constructor
-        self.id_number = id_number  # Unique to Employee
-        self.department = department  # Unique to Employee
-
     # add instance attributes after self, they can be customised after creating the instance
     def __init__(self, firstname, middlename, lastname, email, id_number, department, national_insurance, leave_days=0):
         # use super() to refer to the base class
